7eb36bf8732fd8d1caeb91a5f9176f9e3f7359a9
Peter Jaros <peter.a.jaros@gmail.com>
2009-02-09 09:19:10 -0500

    Move base.rb to spec_helper.rb and add Webrat.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'spec'
+require 'webrat'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../vendor/sequel')
 require 'sequel'
 
 Sequel.sqlite
@@ -11,4 +14,9 @@ require 'ostruct'
 Blog = OpenStruct.new(
 	:title => 'My blog',
 	:author => 'Anonymous Coward'
-)
+) unless defined? Blog
+
+Spec::Runner.configure do |config|
+  config.include Webrat::Matchers
+  config.include Webrat::HaveTagMatcher
+end
