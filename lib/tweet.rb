2ee2f1b8137da54ed3a26495c0280d8199a11d4d
Peter Jaros <peter.a.jaros@gmail.com>
2009-02-23 22:50:32 -0500

    Implement Tweet#to_tweetmark.

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -3,25 +3,27 @@ require 'httparty'
 class Tweet
   include HTTParty
   
-  def self.new
-    t = super
-    yield t
-    t
-  end
-  
-  def self.find(id)
-    t = get("http://twitter.com/statuses/show/#{id}.json")
-    
-    new do |tweet|
-      tweet.id                      = t["id"]
-      tweet.text                    = t["text"]
-      tweet.user_screen_name        = t["user"]["screen_name"]
-      tweet.user_name               = t["user"]["name"]
-      tweet.user_profile_image_url  = t["user"]["profile_image_url"]
-      tweet.created_at              = Time.parse t["created_at"]
-      tweet.source                  = t["source"]
-      tweet.in_reply_to_screen_name = t["in_reply_to_screen_name"]
-      tweet.in_reply_to_status_id   = t["in_reply_to_status_id"]
+  class << self
+    def new
+      t = super
+      yield t
+      t
+    end
+
+    def find(id)
+      t = get("http://twitter.com/statuses/show/#{id}.json")
+
+      new do |tweet|
+        tweet.id                      = t["id"]
+        tweet.text                    = t["text"]
+        tweet.user_screen_name        = t["user"]["screen_name"]
+        tweet.user_name               = t["user"]["name"]
+        tweet.user_profile_image_url  = t["user"]["profile_image_url"]
+        tweet.created_at              = Time.parse t["created_at"]
+        tweet.source                  = t["source"]
+        tweet.in_reply_to_screen_name = t["in_reply_to_screen_name"]
+        tweet.in_reply_to_status_id   = t["in_reply_to_status_id"]
+      end
     end
   end
   
@@ -34,4 +36,16 @@ class Tweet
                 :source,
                 :in_reply_to_screen_name,
                 :in_reply_to_status_id
+  
+  def to_tweetmark
+    <<-TWEETMARK
++twitter:#{id}
+#{text}
+  @#{user_screen_name} (#{user_name}) [#{user_profile_image_url}]
+  at #{created_at.utc}
+  from #{source}
+  in reply to #{in_reply_to_screen_name}:#{in_reply_to_status_id}
+=twitter
+TWEETMARK
+  end
 end
